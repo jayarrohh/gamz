94cc4a91c2c043dd358625eed556bfd3744e237a
jayarrohh <jayarrohh@gmail.com>
2024-07-11 22:49:22 +0100

    Added DirtyWord.csv and GuessWord.csv

--- main.py
+++ main.py
@@ -1,47 +1,66 @@
 import random
 import time
 import tkinter as tk
-from word_list import wordie_words
+from word_list import *
 
-# Choose random word from imported dictionary
-secret_word = random.choice(wordie_words)
+
+# Makes all words in wordlist lower case
+words_lower = []
+for words in wordie_words:
+    words_lower.append(words.lower())
+
+# Random choice from lower case word listpyt
+secret_word = random.choice(words_lower)
 correct_guesses = set()
 incorrect_guesses = set()
 attempts_left = 6
+
 
 class Hangman:
     def __init__(self, master) -> None:
         self.master = master
         self.master.title("CyberFirst Hangman Game")
-        self.master.geomtry("900x600")
+        self.master.geometry("900x600")
+
         # Add More UI stuff here later
 
-#name = input("Hello there! What is your name? ")
+def pick_name():
+    name = input("Hello there! What is your name? ")
+    time.sleep(.5)
+    name_lower = name.lower()
+    
+    if name_lower in banned_cut:
+        print("This name is banned, choose another")
+        pick_name()
+    else:
+        print(f"Nice to meet you, {name}! Let's play Cyber Hangman!")
 
-#time.sleep(1)
-
-#print(f"Nice to meet you, {name}! Let's player Cyber Hangman!")
+pick_name()
 
 
 def display_game_state():
     displayed_word = "".join([letter if letter in correct_guesses else "_" for letter in secret_word])
     print(f"Word: {displayed_word}")
-    print(f"Incorrect Guesses: {" ".join(incorrect_guesses)}")
+    print(f"Incorrect Guesses: {' '.join(incorrect_guesses)}")
     print(f"Attempts Left: {attempts_left}")
 
 # Main loop
+
 while True:
     display_game_state()
-    guess = input("Guess a letter! ").lower
+    guess = input("Guess a letter! ").lower()
 
-    if str(guess) in secret_word:
+    if guess in secret_word:
         correct_guesses.add(guess)
         if set(secret_word).issubset(correct_guesses):
             print("Congratulations! You've won!")
             break
     else:
-        incorrect_guesses.add(guess)
-        attempts_left -= 1
+        if guess not in incorrect_guesses:
+            incorrect_guesses.add(guess)
+            attempts_left -= 1
+        else:
+            print("You already guessed that!")
         if attempts_left == 0:
             print("Womp womp! Better luck next time!")
             print(f"The answer was {secret_word}!")
--- word_list.py
+++ word_list.py
@@ -1,2 +1,23 @@
-wordie_words = ["Cyber Security", "Python", "Malware", "Virus", "Hacker", "Administrator", "Antivrus", 
-                "CyberFirst", "National Cyber Security Centre"]
+import csv
+
+with open("DirtyWords.csv", newline="") as f:
+    reader = csv.reader(f)
+    banned_words = list(reader)
+
+banned_cut = []
+
+# Removes first and last item in each nested list
+for nested in banned_words:
+    banned_cut.append(nested[1])
+
+
+with open("GuessWords.csv", newline="") as f:
+    reader = csv.reader(f)
+    words_orig = list(reader)
+
+wordie_words = []
+
+for nested in words_orig:
+    wordie_words.append(nested[0])
+    
+print(wordie_words)
